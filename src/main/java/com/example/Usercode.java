291c41aff3756fb1457fee2a6bb7a67da839e41a
james1234567890 <jameswachira09@gmail.com>
2022-11-23 10:07:02 +0300

    Update Usercode.java

--- src/main/java/com/example/Usercode.java
+++ src/main/java/com/example/Usercode.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.example;
+package com.corezoid.usercode;
 import com.corezoid.gitcall.runner.api.UsercodeHandler;
 import java.util.UUID;
 
