cc8e35c094f7d248f0f1c0495a1e4a2384a9e897
james1234567890 <jameswachira09@gmail.com>
2022-11-23 10:05:08 +0300

    Update Usercode.java

--- src/main/java/com/example/Usercode.java
+++ src/main/java/com/example/Usercode.java
@@ -3,14 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.example;
-
+import com.corezoid.gitcall.runner.api.UsercodeHandler;
 import java.util.UUID;
 
 /**
  *
  * @author James
  */
-public class Usercode {
+public class Usercode implements UsercodeHandler<String>  {
      public static String generateGUID() {
         UUID uuid = UUID.randomUUID();
         return uuid.toString();
